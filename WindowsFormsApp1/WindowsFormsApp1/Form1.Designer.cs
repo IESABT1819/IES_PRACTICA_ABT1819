844e193093aa7fa8ee2b8042c9491ff31bf33033
IESABT1819 <burruezo19620@iesmarenostrum.com>
2019-04-14 17:31:10 +0200

    Cambios de numericUpDown

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,36 +29,43 @@
         private void InitializeComponent()
         {
             this.aluNombre = new System.Windows.Forms.TextBox();
+            this.aluNota = new System.Windows.Forms.NumericUpDown();
             this.listaAlumnos = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.aluNota = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.aluNota)).BeginInit();
             this.SuspendLayout();
             // 
             // aluNombre
             // 
-            this.aluNombre.Location = new System.Drawing.Point(38, 66);
+            this.aluNombre.Location = new System.Drawing.Point(81, 72);
             this.aluNombre.Name = "aluNombre";
-            this.aluNombre.Size = new System.Drawing.Size(331, 20);
+            this.aluNombre.Size = new System.Drawing.Size(243, 20);
             this.aluNombre.TabIndex = 0;
+            // 
+            // aluNota
+            // 
+            this.aluNota.Location = new System.Drawing.Point(431, 72);
+            this.aluNota.Name = "aluNota";
+            this.aluNota.Size = new System.Drawing.Size(65, 20);
+            this.aluNota.TabIndex = 1;
             // 
             // listaAlumnos
             // 
-            this.listaAlumnos.Location = new System.Drawing.Point(38, 135);
+            this.listaAlumnos.Location = new System.Drawing.Point(81, 127);
             this.listaAlumnos.Multiline = true;
             this.listaAlumnos.Name = "listaAlumnos";
             this.listaAlumnos.ReadOnly = true;
-            this.listaAlumnos.Size = new System.Drawing.Size(731, 303);
+            this.listaAlumnos.Size = new System.Drawing.Size(662, 292);
             this.listaAlumnos.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(631, 63);
+            this.button1.Location = new System.Drawing.Point(608, 69);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 23);
+            this.button1.Size = new System.Drawing.Size(135, 23);
             this.button1.TabIndex = 3;
             this.button1.Text = "Guardar alumno";
             this.button1.UseVisualStyleBackColor = true;
@@ -66,7 +73,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 50);
+            this.label1.Location = new System.Drawing.Point(78, 56);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(81, 13);
             this.label1.TabIndex = 4;
@@ -75,7 +82,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(475, 49);
+            this.label2.Location = new System.Drawing.Point(428, 56);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(30, 13);
             this.label2.TabIndex = 5;
@@ -84,30 +91,23 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(35, 119);
+            this.label3.Location = new System.Drawing.Point(78, 111);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.Size = new System.Drawing.Size(81, 13);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Lista de alumnos";
-            // 
-            // aluNota
-            // 
-            this.aluNota.Location = new System.Drawing.Point(456, 65);
-            this.aluNota.Name = "aluNota";
-            this.aluNota.Size = new System.Drawing.Size(49, 20);
-            this.aluNota.TabIndex = 7;
+            this.label3.Text = "Lista de alumno";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listaAlumnos);
+            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.aluNombre);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -120,12 +120,12 @@
         #endregion
 
         private System.Windows.Forms.TextBox aluNombre;
+        private System.Windows.Forms.NumericUpDown aluNota;
         private System.Windows.Forms.TextBox listaAlumnos;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.NumericUpDown aluNota;
     }
 }
 
